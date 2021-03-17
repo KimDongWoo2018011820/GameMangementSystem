d0da95c952ede50e4a7cf21362a355c677f63549
KimDongWoo2018011820 <ehddn1563@gmail.com>
2021-03-17 16:14:34 +0900

    removed hello.jave and added MenuManger.java

---
+++ src/MenuManger.java
@@ -0,0 +1,14 @@
+
+public class MenuManger {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Game");
+		System.out.println("2. Delete Game");
+		System.out.println("3. Edit Game");
+		System.out.println("4. View Game");
+		System.out.println("5. Exit");
+		
+
+	}
+
+}
