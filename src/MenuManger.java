b88545af45ca8f38757b8a6188df90514e8a5508
KimDongWoo2018011820 <ehddn1563@gmail.com>
2021-04-06 00:32:28 +0900

    class project : Using another class ex) Game, Game,manger

--- src/MenuManger.java
+++ src/MenuManger.java
@@ -3,74 +3,39 @@ import java.util.Scanner;
 public class MenuManger {
 
 	public static void main(String[] args) {
-		int num =0;
-		int input_time=0;
 	    Scanner input= new Scanner(System.in);
+	    GameManger gameManger = new GameManger(input);
+	    int num =0;
 		
 	    //Menu
-		while (num != 6){
+		while (num != 5){
 		    System.out.println("< GameMangementSystem >");
 		    System.out.println(" 1. Add Game");
 		    System.out.println(" 2. Delete Game");
 		    System.out.println(" 3. Edit Game");
 		    System.out.println(" 4. View Game");
-		    System.out.println(" 5. Show a menu");
-		    System.out.println(" 6. Exit");
-		    System.out.print("Select one number between 1-6 :");
+		    System.out.println(" 5. Exit");
+		    System.out.print("Select one number between 1-5 :");
 		    num = input.nextInt();
 		    
 		    //interaction
 		   switch (num) {
 		   case 1:
-		       AddGame();
+			   gameManger.AddGame();
 	    	   break;
 	       case 2:
-	    	   DeleteGame();
+	    	   gameManger.DeleteGame();
 	           break;
 		   case 3:
-			   EditGame();
+			   gameManger.EditGame();
 			   break;
 		   case 4:
-		       ViewGame();
+			   gameManger.ViewGame();
 		       break;
 		}
 	}
 
   }
-    public static void AddGame() {
-    	Scanner input = new Scanner(System.in);
-    	System.out.print("Game Name :");
-	    String Game_name = input.nextLine();	
-	    System.out.print("Game Type :");
-	    String Game_type = input.next();	
-	    System.out.print("Game Playtime :");
-	    int Game_playtime = input.nextInt();	
-	    System.out.print("Game Distributor :");
-	    String Game_distributor = input.next();	
-	    System.out.print("Game Price :");
-	    double Game_price = input.nextDouble();
-    }
-    public static void DeleteGame() {
-    	Scanner input = new Scanner(System.in);
-    	System.out.print("Game Name :");
-	    String Game_name = input.nextLine();	
-    	
-    	
-    }
-    
-    public static void EditGame() {
-    	Scanner input = new Scanner(System.in);
-    	System.out.print("Game Name :");
-	    String Game_name = input.nextLine();	
-    	
-    	
-    }
    
-    public static void ViewGame() {
-    	Scanner input = new Scanner(System.in);
-    	System.out.print("Game Name :");
-	    String Game_name = input.nextLine();
-    	
-    }
     
 }
