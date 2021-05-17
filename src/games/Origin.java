17a9aee9833f1c48458b19623700b9260daa4213
KimDongWoo2018011820 <ehddn1563@gmail.com>
2021-05-17 20:04:43 +0900

    11th week oop

--- src/games/Origin.java
+++ src/games/Origin.java
@@ -1,8 +1,60 @@
 package games;
 
-public class Origin extends Game {
-	public Origin ( GameKinds kind ) {
-		this.kind = kind;	
+import java.util.Scanner;
+
+public class Origin extends Game implements GameInput {
+	
+	public Origin( GameKinds kind ) {
+		this.platform = kind;	
 	}
+	
+	
+	public void getUserinput(Scanner input) {
+		System.out.print("Game Name :");
+	    String name = input.next();	
+	    this.setName(name);
+	   
+	    System.out.print("Game Type :");
+	    String type = input.next();	
+	    this.setType(type);
+	   
+	    System.out.print("Game Playtime :");
+	    int playtime = input.nextInt();	
+	    this.setPlaytime(playtime);
+	   
+	    System.out.print("Game Distributor :");
+	    String distributor = input.next();
+	    this.setDistributor(distributor);
+	   
+	    System.out.print("Game Price :");
+	    double price = input.nextDouble();
+	    this.setPrice(price);
+	}
+	
+	
+	public void printinfo() {
+		String skind = "no kins"; 
+		switch(this.platform) {
+		case Origin :
+			skind = "Origin";
+			break;
+		case Steam:
+			skind = "Steam";
+			break;
+		case Ubisoft :
+			skind = "Ubisoft";
+			break;
+		case Nexon:
+			skind = "Nexon";
+			break;
+		default :					
+		}
+		System.out.println("platform : "+skind+ " name : "+ name+" type : "+ type+" playtime : "+ playtime+" distributor : "+ distributor+" price : "+ price+"$");
+	}
+	
+	
+	
+	
+	
 
 }
