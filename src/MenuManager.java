95fcf491231f7d42f3b456aa9fcbd135315cd12e
KimDongWoo2018011820 <ehddn1563@gmail.com>
2021-05-25 10:28:00 +0900

    12th week oop project

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,9 +1,13 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import log.EventLogger;
+
 public class MenuManager {
+	static EventLogger logger = new EventLogger("log.txt");
 
 	public static void main(String[] args) {
+		
 	    Scanner input= new Scanner(System.in);
 	    GameManager gameManger = new GameManager(input);
 	    selectMenu(input,gameManger);
@@ -18,17 +22,22 @@ public class MenuManager {
 				 switch (num) {
 				   case 1:
 					   gameManger.AddGame();
+					   logger.log("Add students");
 			    	   break;
 			       case 2:
 			    	   gameManger.DeleteGame();
+			    	   logger.log("Delete students");
 			           break;
 				   case 3:
 					   gameManger.EditGame();
+					   logger.log("Edit students");
 					   break;
 				   case 4:
 					   gameManger.ViewGames();
+					   logger.log("View students");
 				       break;
 				   default:
+					   logger.log("EXIT");
 					   continue;
 				}
 			 }catch(InputMismatchException e) {
